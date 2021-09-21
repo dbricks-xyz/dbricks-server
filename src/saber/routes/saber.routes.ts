656dcc3858ae7a57df79029bacef1bfd4213f41a
Steven Li <steven4354@gmail.com>
2021-09-20 22:11:22 -0700

    saber routes

---
+++ src/saber/routes/saber.routes.ts
@@ -0,0 +1,50 @@
+import e from 'express';
+import CommonRoutesConfig from '../../common/routes/common.routes.config';
+// TODO: update to saber
+import SerumMiddleware from '../../serum/middleware/serum.middleware';
+import SerumController from '../../serum/controller/serum.controller';
+
+export class SaberRoutes extends CommonRoutesConfig {
+  constructor(app: e.Application) {
+    super(app, 'SaberRoutes');
+  }
+
+  configureRoutes(): e.Application {
+    // --------------------------------------- orders
+    this.app.route('/saber/orders')
+      .get() // todo returns all orders
+      .post(
+        SerumMiddleware.validateStuff,
+        SerumController.placeOrder,
+      );
+
+    this.app.route('/saber/orders/cancel')
+      .post(
+        SerumMiddleware.validateStuff,
+        SerumController.cancelOrder,
+      );
+
+    // --------------------------------------- markets
+
+    this.app.route('/saber/markets')
+      .get() // todo return all markets
+      .post(
+        SerumMiddleware.validateStuff,
+        SerumController.initMarket,
+      );
+
+    this.app.route('/saber/markets/settle')
+      .post(
+        SerumMiddleware.validateStuff,
+        SerumController.settleMarket,
+      );
+
+    this.app.route('/saber/markets/basequote')
+      .post(
+        SerumMiddleware.validateStuff,
+        SerumController.getBaseQuote
+      )
+
+    return this.app;
+  }
+}
