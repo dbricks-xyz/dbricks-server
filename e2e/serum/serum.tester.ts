69f6ff9c0e1a75e2d070b073f762f90d5f8ab28d
ilmoi <iljamoi@protonmail.com>
2021-09-29 17:02:08 +0300

    small docs generation fix for serum

--- e2e/serum/serum.tester.ts
+++ e2e/serum/serum.tester.ts
@@ -143,7 +143,7 @@ export default class SerumTester extends SerumClient {
     return deserializeInstructionsAndSigners(response.body);
   }
 
-  async requestCancelOrderInstruction(orderId: string, ownerPubkey: string) {
+  async requestCancelOrderInstruction(orderId: string, ownerPubkey: string, saveRes = true) {
     const route = '/serum/orders/cancel';
     const params: ISerumDEXOrderCancelParams = {
       marketPubkey: this.marketKeypair.publicKey.toBase58(),
@@ -151,14 +151,16 @@ export default class SerumTester extends SerumClient {
       ownerPubkey,
     };
     const response = await request(app).post(route).send(params).expect(200);
-    saveRequestResponseToJSON(
-      'serum.orders.cancel',
-      'serum',
-      'POST',
-      route,
-      params,
-      response.body
-    );
+    if (saveRes) {
+      saveRequestResponseToJSON(
+        'serum.orders.cancel',
+        'serum',
+        'POST',
+        route,
+        params,
+        response.body
+      );
+    }
     return deserializeInstructionsAndSigners(response.body);
   }
 
@@ -176,10 +178,11 @@ export default class SerumTester extends SerumClient {
     await this._prepareAndSendTransaction(transaction);
   }
 
-  async cancelOrder(user: Keypair, orderId: string) {
+  async cancelOrder(user: Keypair, orderId: string, saveRes=true) {
     const cancelTransaction = (await this.requestCancelOrderInstruction(
       orderId,
       user.publicKey.toBase58(),
+      saveRes,
     ))[0];
     cancelTransaction.signers.unshift(this.user1Keypair);
     await this._prepareAndSendTransaction(cancelTransaction);
