563bce53478f1ebe0f915939823faec48fc82ca1
ilmoi <iljamoi@protonmail.com>
2021-10-13 18:27:12 +0300

    fixed dist folder missing config files

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import http from 'http';
-import debug from 'debug';
 import CommonRoutesConfig from './common/routes/common.routes.config';
-import app, { routes } from './app';
-
-const log: debug.IDebugger = debug('app');
+import app, {routes} from './app';
 
 const server: http.Server = http.createServer(app);
 const port = 3000;
 
 server.listen(port, () => {
   routes.forEach((route: CommonRoutesConfig) => {
-    log(`Routes configured for ${route.getName()}`);
+    console.log(`Routes configured for ${route.getName()}`);
   });
-  log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
